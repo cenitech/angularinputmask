a04206222af7bb8850bab08f7a756ecb02c107c4
lceni <lceni@users.noreply.github.com>
2015-10-22 00:25:43 -0200

    Update cenitech.angularinputmask.js

--- cenitech.angularinputmask.js
+++ cenitech.angularinputmask.js
@@ -3,7 +3,7 @@
  * Author: Lucas Ceni                     *
  * Email: lucas.ceni at gmail             *
  * Date: 2015                             *
- * github: https://github.com/lceni   *
+ * github: https://github.com/lucasceni   *
  *                                        *
  ******************************************/
 
@@ -12,16 +12,15 @@
 angular.module('cenitech.angularinputmask', [])
     .directive('inputmask', inputmask);
 
-function inputmask() {
+function inputmask($timeout) {
     return {
         restrict: 'A',
         require: 'ngModel',
         link: function (scope, elem, attrs, controller) {
             var toUpdate = true;
-    
             scope.$watch(attrs.ngModel, function(newValue, oldValue) {
                 if (toUpdate) {
-                    $(elem).val(newValue);   
+                    $(elem).val(newValue);
                 }
                 toUpdate = true;
             });
@@ -32,25 +31,9 @@ function inputmask() {
                 controller.$setViewValue(elem.val());
             });
 
-            // elem.bind('keyup', function() {
-            //      controller.$setViewValue(elem.val());
-            //     // do not call $render() since cursor will make this move to end
-            // });
-
-            // elem.bind('focus', function() {
-            //     controller.$setViewValue(elem.val());
-            //     controller.$render();
-            // });
-
-            // elem.bind('blur', function() {
-            //     controller.$setViewValue(controller.$viewValue);
-            //     controller.$render();
-
-            //     if (controller.$viewValue && controller.$viewValue.length > 0) {
-            //         var value = controller.$viewValue;
-            //         controller.$setValidity('', value && value.indexOf('_') > -1 ? false : true);
-            //     }
-            // });
+            $(elem).blur(function() {
+                toUpdate = true;
+            });
         }
     };
 }
